8daa250805bcabb042d70292db528a7042ed4bd3
Alejandrorusinoff <74211057+Alejandrorusinoff@users.noreply.github.com>
2023-05-09 10:45:48 -0300

    Update env.js

--- client/src/env.js
+++ client/src/env.js
@@ -1,2 +1,2 @@
 export const API_URL = 'http://localhost:3001/api'
-export const REACT_APP_OWNER_PRIVATE_KEY = '2442480bfcba0db3303f26c06cd8f47421e8362992e538c75fc7373745235dfd'
+export const REACT_APP_OWNER_PRIVATE_KEY = ''
